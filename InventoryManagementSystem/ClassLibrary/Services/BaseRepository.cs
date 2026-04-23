ebd3a43a0dc3e7e32fbdbe673ff8fd3d507a5fa9
Gehad Ahmed <gehad2363@gmail.com>
2026-04-23 23:11:18 +0200

    Add Folders For organization

---
+++ InventoryManagementSystem/ClassLibrary/Services/BaseRepository.cs
@@ -0,0 +1,31 @@
+﻿using Class_Library.Context;
+using Class_Library.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Library.Services
+{
+    public abstract class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        protected readonly InventoryManagementContext _context;
+
+        public BaseRepository(InventoryManagementContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
+
+        public async Task<T> GetByIdAsync(int id) => await _context.Set<T>().FindAsync(id);
+
+        public async Task AddAsync(T entity) => await _context.Set<T>().AddAsync(entity);
+
+        public void Delete(T entity) => _context.Set<T>().Remove(entity);
+
+        public abstract void Update(T entity);
+    }
+}
